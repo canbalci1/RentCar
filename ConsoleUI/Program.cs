25f790307708e66a21f63d921efd4feaef9a2330
Can Balcı <canbalci@outlook.com.tr>
2021-11-01 16:00:27 +0300

    01.11.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,36 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var car in carManager.GetCarsByColorId(2))
+            CarTest();
+            //BrandTest();
+            //ColorTest();
+
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            foreach (var color in colorManager.GetAll())
             {
-                Console.WriteLine(car.ColorId);
+                Console.WriteLine(color.Name);
+            }
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.Name);
+            }
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine(car.CarName+ "-" + car.BrandName + "-" + car.ColorName + "-" + car.DailyPrice);
             }
         }
     }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,55 +11,26 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarsContext>, ICarDal
     {
-        public void Add(Car entity)
-        {
-            using (CarsContext context=new CarsContext())
-            {
-                var addEntity = context.Entry(entity);
-                addEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Car entity)
+        public List<CarDetailDto> GetCarDetails()
         {
             using (CarsContext context = new CarsContext())
             {
-                var deleteEntity = context.Entry(entity);
-                deleteEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }      
+                var result = from c in context.Cars
+                             join b in context.Brands on c.Id equals b.Id
+                             join co in context.Colors on c.ColorId equals co.Id
 
-        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
-        {
-            using (CarsContext context = new CarsContext())
-            {
-                return filter == null
-                    ? context.Set<Car>().ToList()
-                    : context.Set<Car>().Where(filter).ToList();                
-            }
-        
-        }
 
-        public void Update(Car entity)
-        {
-            using (CarsContext context = new CarsContext())
-            {
-                var updateEntity = context.Entry(entity);
-                updateEntity.State = EntityState.Modified;
-                context.SaveChanges();
+                             select new CarDetailDto { 
+                                 CarName = c.CarName, 
+                                 BrandName = b.Name, 
+                                 ColorName = co.Name, 
+                                 DailyPrice = c.DailyPrice };
+                return result.ToList();
             }
-        }
 
-        Car IEntityRepository<Car>.Get(Expression<Func<Car, bool>> filter)
-        {
-            using (CarsContext context = new CarsContext())
-            {
-                return context.Set<Car>().SingleOrDefault(filter);
-            }
+            
         }
     }
 }
